06af9cb176b637f6fc945c0775060461ac0cf784
Rafa <rafael.rodriguez@uca.es>
2015-03-22 18:03:30 +0100

    Added recent files and use-package

--- init.el
+++ init.el
@@ -11,5 +11,30 @@
 ;; Directoy for local emacs lisp files:  ~/.emacs.d/site-lisp/
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/setup"))
 
+;; Recent files
+(require 'recentf)
+(setq recentf-max-saved-items 200
+      recentf-max-menu-items 15)
+(recentf-mode)
+
+;;
+;; Package management
+;; ------------------------------------------------------------
+;;
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+
+(package-initialize)
+
+;; In current emacs configuration we will employ 'use-package' for
+;; install and config packages. See https://github.com/jwiegley/use-package
+
+;; Bootstrap `use-package'
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+
 ;; Fix bug with dead-keys in Ubuntu 13.10, 14.04, 14.10?
 (require 'iso-transl)
