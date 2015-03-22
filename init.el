d30c79d25925b9b6cb5a4de1635bf62351366113
Rafa <rafael.rodriguez@uca.es>
2015-03-22 18:30:54 +0100

    Personalize basic emacs behaviour

--- init.el
+++ init.el
@@ -5,13 +5,78 @@
 ;;
 ;; https://bitbucket.org/proyecto-ucaccar/emacs-easy-config
 
+;;
+;; Personalize basic emacs behaviour. See https://github.com/rrgalvan/emacs
+;; -------------------------------------------------------------------
+
 ;; No startup screen
 (setq inhibit-startup-message t)
 
 ;; Directoy for local emacs lisp files:  ~/.emacs.d/site-lisp/
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/setup"))
 
+;; Highlight marked text - only works under X.
+(transient-mark-mode t)
+
+;; Remove text in active region if inserting text
+(delete-selection-mode 1)
+
+;; In programming buffers, distinguish CamelCase sub-words
+(add-hook 'prog-mode-hook 'subword-mode)
+
+;; Scroll by one line at a time.
+(setq scroll-step 1)
+
+;; Scroll
+(global-set-key [S-down] 'scroll-up-line)
+(global-set-key [S-up]  'scroll-down-line)
+
+;; Make searches case insensitive.
+(setq case-fold-search nil)
+
+;; Turn on highlighting for search strings.
+(setq search-highlight t)
+
+;; Remove trailing whitespace automatically
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+;; Down-arrow at the end of a file doesn't add in a new line.
+(setq next-line-add-newlines nil)
+
+;; Silently ensure a file ends in a newline when it's saved.
+(setq require-final-newline t)
+
+;; Show me empty lines after buffer end
+(set-default 'indicate-empty-lines t)
+
+;; Add parts of each file's directory to the buffer name if not unique
+(require 'uniquify)
+(setq uniquify-buffer-name-style 'forward)
+
+;; Make script files executable automatically
+;; http://www.masteringemacs.org/articles/2011/01/19/script-files-executable-automatically/
+(add-hook 'after-save-hook 'executable-make-buffer-file-executable-if-script-p)
+
+;; "All strings representing colors will be highlighted with the color they represent."
+(rainbow-mode t)
+
+;; Include the size of the file in the mode line
+(size-indication-mode nil)
+
+;; Show which column I'm in in the mode line as well
+(column-number-mode t)
+
+;; 8 is wrong
+(setq tab-width 4)
+
+;; Make the cursor a thin bar, not a block (but I still like it blinking)
+;;(setq cursor-type 'bar)
+
+;; M-x compile is tedious
+(global-set-key (kbd "C-c c") 'compile)
+
 ;; Recent files
+;; -------------------------------------------------------------------
 (require 'recentf)
 (setq recentf-max-saved-items 200
       recentf-max-menu-items 15)
@@ -19,7 +84,7 @@
 
 ;;
 ;; Package management
-;; ------------------------------------------------------------
+;; -------------------------------------------------------------------
 ;;
 (require 'package)
 (setq package-enable-at-startup nil)
